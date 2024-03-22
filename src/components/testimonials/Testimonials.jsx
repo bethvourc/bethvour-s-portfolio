f78904d712d5b27db7ae7518dcfea56c42891000
Bethvour <bethvourc@gmail.com>
2024-03-22 00:39:34 -0500

    Profile update

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -19,31 +19,31 @@ import "swiper/css/pagination";
 const data = [
   {
     avatar: AVTR1,
-    name: "John Doe 1",
+    name: "Dean",
     review:
       "Bethvour comes up with ideas easily, and his ability to question the status quo is impressive. In our daily work, he drives our team not only with constant contribution but also with his visionary mind.",
   },
   {
     avatar: AVTR2,
-    name: "Jane Doe 1",
+    name: "Sarah",
     review:
-      "We worked closely in a team over a periode of four months, and I couldn't have asked for a better partner! Bethvour is extremely dedicated, and always askes the right questions in order to be on the right track.",
+      "We worked closely in a team over a period of four months, and I couldn't have asked for a better partner! Bethvour is extremely dedicated, and always askes the right questions in order to be on the right track.",
   },
   {
     avatar: AVTR3,
-    name: "Jane Doe 2",
+    name: "Elizabeth",
     review:
       "I had the pleasure of working with Bethvour on an extensive project and Bethvour is very tidy, structured and keen to find good solutions for problems. For me, he has been a good partner and I have benefited a lot from discussing with him.",
   },
   {
     avatar: AVTR4,
-    name: "John Doe 2",
+    name: "Emily",
     review:
       "I worked with Bethvour on a challenging trend analysis project. I would describe him as solution-oriented and adaptable. Last but not least, he is creative, good at design, and a great collaborator!",
   },
   {
     avatar: AVTR5,
-    name: "John Doe 3",
+    name: "Zaheed",
     review:
       "I had the pleasure of working with Bethvour on a very challenging project! He is good at finding simple and creative solutions to complex challenges. He is also very helpful, motivated and has great work capacity.",
   },
