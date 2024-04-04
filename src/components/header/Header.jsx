e75f97242f8c0e8c67193df717f661ebab2f0970
Bethvour <bethvourc@gmail.com>
2024-04-04 10:03:26 -0500

    updated description

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Bethvour Chike</h1>
-        <h5 className="text-light">Full Stack Developer</h5>
+        <h5 className="text-light">Software Engineer</h5>
         <CTA />
         <HeaderSocials />
         <div className="ademir">
